877f231252ec36ed5d05178731aca6570418e6c7
winicius-ol <winiciusoliveira3@gmail.com>
2025-06-25 19:29:54 -0300

    Add SPECIFIED_USER_ID to environment variables and enhance message handling with reactions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from discord.ext import commands, tasks
-import os
+import os, random, asyncio
 
+SPECIFIED_USER_ID = int(os.environ['SPECIFIED_USER_ID'])
 CRON_JOB_HOURS = int(os.environ['CRON_JOB_HOURS'])
 POKEMON_CHANNEL = int(os.environ['POKEMON_CHANNEL'])
 DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
-
+EMOJIS = ['❤️', '💖', '💗', '💓', '💕', '💞', '💘', '😍', '🥰', '😘', '😻']
 
 bot = commands.Bot(command_prefix='Nayeon ', bot=False)
 
@@ -23,6 +24,16 @@ async def on_ready():
     print(f'Bot is logged in as {bot.user}')
     if not cronjob1.is_running():
         cronjob1.start()
+
+
+@bot.event
+async def on_message(message):
+    is_expected_user_message = (message.author.id == SPECIFIED_USER_ID and message.content == "$p")
+    if is_expected_user_message and random.random() < 0.3:
+        await asyncio.sleep(random.uniform(0.5, 1))
+        await message.add_reaction(random.choice(EMOJIS))
+
+    await bot.process_commands(message)
 
 
 @bot.command()
